3a0457bf1f778357db7618038737aab639c0934e
Sodiq Sanusi <ademilddiq@gmail.com>
2022-09-07 21:09:03 +0100

    Done with the email validation and error handling (Mobile)
    This should be the end of most things mobile, 'should be', lool.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const navButton = document.getElementById('mobileNavButton');
 const navButtonImage = document.querySelector('#mobileNavButton img');
 const mobileNavPanel = document.querySelector('.mobileNav');
+const emailForm = document.querySelector('.input-email');
+const userInput = document.querySelector('.input-email input');
+const errMessage = document.getElementById('err-message');
 
 navButton.onclick = () => {
   let source = navButtonImage.getAttribute('src')
@@ -14,3 +17,14 @@ navButton.onclick = () => {
   mobileNavPanel.style.display = 'none';
   document.body.style.overflow = 'initial';
 }
+
+emailForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if(!/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi.test(userInput.value)){
+    userInput.classList.add('error');
+    errMessage.style.display = 'block';
+    return;
+  }
+  userInput.classList.remove('error');
+  errMessage.style.display = 'none';
+})
